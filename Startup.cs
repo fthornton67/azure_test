90fdba7904961a0b8e5e09c14d159b9af51cb8f1
fthornton67 <fthornton67@gmail.com>
2018-04-10 10:30:07 -0700

    update

--- Startup.cs
+++ Startup.cs
@@ -1,34 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Owin;
 
-namespace phitus_leads
+namespace TodoListService
 {
-    public class Startup
+    public partial class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void Configuration(IAppBuilder app)
         {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello Phitr!");
-            });
+            ConfigureAuth(app);
         }
     }
 }

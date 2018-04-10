6dcd614d8db1a747012a0a0fd51d151e190d0112
fthornton67 <fthornton67@gmail.com>
2018-04-10 10:04:21 -0700

    update test

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace phitus_leads
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Phitr!");
             });
         }
     }
